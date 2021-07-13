2026302b4e811e942f59c5fbe739d3998241936b
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-07-13 00:17:49 -0300

    new structure to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="home container">
-    <div class="card-area">
-      <div class="wrap-card"
-        v-for="(char, index) in characters.results" :key="index">
-        <div class="card">
-          <p class="char-name">{{ char.name }}</p>
-          <img class="image-card" :src="char.image">
-        </div>
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import gql from 'graphql-tag';
-
-
 export default {
   name: 'Home',
-  apollo: {
-    characters: gql `query {
-      characters {
-        results {
-          name
-          image
-        }
-      }
-    }`
-  },
 };
 </script>
