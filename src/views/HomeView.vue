b4c3dd55b0baed3d62c8afd310255ebc507e16dd
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-07-13 00:17:55 -0300

    new home index

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card-area">
+    <div class="wrap-card"
+      v-for="(char, index) in characters.results" :key="index">
+      <router-link :to="{ name: 'character', params: { id: char.id } }">
+        <card-character :data="char"></card-character>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+import CardCharacterVue from '../components/cards/CardCharacter.vue';
+
+export default {
+  name: 'index',
+  components: {
+    'card-character': CardCharacterVue,
+  },
+  apollo: {
+    characters: gql `query {
+      characters {
+        results {
+          name
+          image
+          id
+        }
+      }
+    }`
+  },
+}
+</script>
