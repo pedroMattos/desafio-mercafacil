5eabdd03881b1e2cb26b80b7c44714b97e1ef37b
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-07-13 00:16:55 -0300

    char detail view

---
+++ src/views/CharacterDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <p>{{ $route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  name: 'character-detail',
+
+  mounted() {
+    console.log(this.$route.params.id)
+  },
+  // apollo: {
+  //   characters: gql `query {
+  //     charactersByIds(ids: ${this.$route.params.id}) {
+  //       results {
+  //         name
+  //         image
+  //         id
+  //       }
+  //     }
+  //   }`
+  // },
+}
+</script>
