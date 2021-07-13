41b04314703a40e291db6a7273c4978f667ca314
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-07-13 00:17:21 -0300

    new component

---
+++ src/components/cards/CardCharacter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card">
+    <p class="char-name">{{ data.name }}</p>
+    <div class="overlay"></div>
+    <img class="image-card" :src="data.image" :alt="data.name">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card-char',
+  props: ['data'],
+};
+</script>
